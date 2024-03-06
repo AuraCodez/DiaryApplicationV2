504d0780114e0fe00c0352b2e3d4f3df00a717c1
Ryan <86922669+AuraCodez@users.noreply.github.com>
2024-03-06 18:44:54 -0500

    added navigation files

---
+++ app/src/main/java/com/example/diaryapplicationv2/constants/Constant.kt
@@ -0,0 +1,7 @@
+package com.example.diaryapplicationv2.constants
+
+object Constant {
+    const val APPLICATION_ID = "application-0-dusht"
+    const val CLIENT_ID = "252947187658-bep3nlsbfel4l6k7595s2jeci0t50fho.apps.googleusercontent.com"
+    const val WRITE_SCREEN_PARAM_KEY = "diaryId"
+}
---
+++ app/src/main/java/com/example/diaryapplicationv2/navigation/NavGraph.kt
@@ -0,0 +1,4 @@
+package com.example.diaryapplicationv2.navigation
+
+class NavGraph {
+}
